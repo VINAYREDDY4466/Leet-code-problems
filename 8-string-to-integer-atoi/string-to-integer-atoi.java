2074b1df2d37fb235f9bf666b9c6e6d556704b5c
VINAYREDDY4466 <118588257+VINAYREDDY4466@users.noreply.github.com>
2024-07-07 10:34:23 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (39.83%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -3,27 +3,22 @@ class Solution {
         int i = 0, n = s.length();
         int sign = 1;
         int result = 0;
-        
-        // Step 1: Ignore leading whitespace
+     
         while (i < n && s.charAt(i) == ' ') {
             i++;
         }
         
-        // Step 2: Check for sign
+  
         if (i < n && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
             sign = (s.charAt(i) == '-') ? -1 : 1;
             i++;
         }
-        
-        // Step 3: Convert the digits
+       
         while (i < n && Character.isDigit(s.charAt(i))) {
             int digit = s.charAt(i) - '0';
-            
-            // Check for overflow and underflow
-            if (result > (Integer.MAX_VALUE - digit) / 10) {
+             if (result > (Integer.MAX_VALUE - digit) / 10) {
                 return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-            
+            }    
             result = result * 10 + digit;
             i++;
         }
