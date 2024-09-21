9a55096b93af10a501488c181daa2765687b029a
VINAYREDDY4466 <118588257+VINAYREDDY4466@users.noreply.github.com>
2024-09-21 17:22:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (62.93%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,24 +1,18 @@
 class Solution {
-    public int solve(int[] arr , int [] dp, int ind){
-        if(ind<0){
-            return 0;
-        }
-        if(ind==0){
-            return arr[ind];
-
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        int pick= arr[ind]+ solve(arr, dp, ind-2);
-        int notpick= solve(arr, dp ,ind-1);
-        return dp[ind] =Math.max(pick, notpick);
-    }
+   
     public int rob(int[] arr) {
-        int n= arr.length;
-        int[] dp= new int[n];
-        Arrays.fill(dp, -1);
-        int ans= solve(arr, dp, n-1);
-        return ans;
+       int n=arr.length;
+       int[] dp= new int[n+1];
+       dp[0]=arr[0];
+       int notpick= Integer.MAX_VALUE;
+       for(int i=1;i<n;i++){
+          int pick=arr[i];
+          if(i>1){
+            pick += dp[i-2];
+          }
+          notpick =dp[i-1];
+          dp[i]=Math.max(pick, notpick);
+       }
+       return dp[n-1];
     }
 }
